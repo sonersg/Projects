c77844e6608d5a9755ee40d72056b2041648b4b4
sonersg <sonersg@outlook.com>
2022-09-11 22:39:37 +0300

    Add files via upload

--- Vanilla JS Projects/06-Meme Generator/app.js
+++ Vanilla JS Projects/06-Meme Generator/app.js
@@ -2,56 +2,92 @@
 // local memes
 const memes = [
     {
-        img: "./images/1.jpg",
+        img: "./images/1.png",
         info: "#1:"
     },
     {
-        img: "./images/2.jpg",
+        img: "./images/2.png",
         info: "#2:"
     },
     {
-        img: "./images/3.jpg",
+        img: "./images/3.png",
         info: "#3:"
     },
     {
-        img: "./images/4.jpeg",
+        img: "./images/4.png",
         info: "#4:"
     },
     {
-        img: "./images/5.jpg",
+        img: "./images/5.png",
         info: "#5:"
     },
     {
-        img: "./images/6.jpg",
+        img: "./images/6.png",
         info: "#6:"
     },
     {
-        img: "./images/7.jpeg",
+        img: "./images/7.png",
         info: "#7:"
     },
     {
-        img: "./images/8.jpeg",
+        img: "./images/8.png",
         info: "#8:"
     },
     {
-        img: "./images/9.jpeg",
+        img: "./images/9.png",
         info: "#9:"
     },
     {
-        img: "./images/10.jpeg",
+        img: "./images/10.png",
         info: "#10:"
     },
     {
-        img: "./images/11.jpg",
+        img: "./images/11.png",
         info: "#11:"
     },
     {
-        img: "./images/12.jpg",
+        img: "./images/12.png",
         info: "#12:"
     },
     {
         img: "./images/13.png",
         info: "#13:"
+    },
+    {
+        img: "./images/14.png",
+        info: "#14:"
+    },
+    {
+        img: "./images/15.png",
+        info: "#15:"
+    },
+    {
+        img: "./images/16.png",
+        info: "#16:"
+    },
+    {
+        img: "./images/17.png",
+        info: "#17:"
+    },
+    {
+        img: "./images/18.png",
+        info: "#18:"
+    },
+    {
+        img: "./images/19.png",
+        info: "#19:"
+    },
+    {
+        img: "./images/20.png",
+        info: "#20:"
+    },
+    {
+        img: "./images/21.png",
+        info: "#21:"
+    },
+    {
+        img: "./images/22.png",
+        info: "#22:"
     }
 ];
 
