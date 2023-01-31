36c9e4457467699d8a3d78e899bef02be1ab9b10
sonersg <sonersg@outlook.com>
2023-01-31 22:19:40 +0300

    Add files via upload

---
+++ React Weather App/timeZones.js
@@ -0,0 +1,158 @@
+export const timeZones = {
+  "-43200": {
+    timeZone: "International Date Line West",
+    UTC: "-12:00",
+  },
+  "-39600": {
+    timeZone: "Midway Island, Samoa",
+    UTC: "-11:00",
+  },
+  "-36000": {
+    timeZone: "Hawaii",
+    UTC: "-10:00",
+  },
+  "-34200": {
+    timeZone: "French Polynesia, Marquesas Islands",
+    UTC: "-09:30",
+  },
+  "-32400": {
+    timeZone: "Alaska",
+    UTC: "-09:00",
+  },
+  "-28800": {
+    timeZone: "Pacific Time (US & Canada)",
+    UTC: "-08:00",
+  },
+  "-25200": {
+    timeZone: "Mountain Time (US & Canada)",
+    UTC: "-07:00",
+  },
+  "-21600": {
+    timeZone: "Central Time (US & Canada), Guadalajara, Mexico city",
+    UTC: "-06:00",
+  },
+  "-18000": {
+    timeZone: "Eastern time (US & Canada)",
+    UTC: "-05:00",
+  },
+  "-16200": {
+    timeZone: "Venezuela",
+    UTC: "-04:30",
+  },
+  "-14400": {
+    timeZone: "Atlantic time (Canada), Manaus, Santiago",
+    UTC: "-04:00",
+  },
+  "-12600": {
+    timeZone: "Newfoundland",
+    UTC: "-03:30",
+  },
+  "-10800": {
+    timeZone: "Greenland, Brasilia, Montevideo",
+    UTC: "-03:00",
+  },
+  "-7200": {
+    timeZone: "Mid-Atlantic",
+    UTC: "-02:00",
+  },
+  "-3600": {
+    timeZone: "Azores",
+    UTC: "-01:00",
+  },
+  0: {
+    timeZone: "GMT: Dublin, Edinburgh, Lisbon, London",
+    UTC: "00:00",
+  },
+  3600: {
+    timeZone: "Amsterdam, Berlin, Rome, Vienna, Prague, Brussels",
+    UTC: "+01:00",
+  },
+  7200: {
+    timeZone: "Athens, Istanbul, Beirut, Cairo, Jerusalem",
+    UTC: "+02:00",
+  },
+  10800: {
+    timeZone: "St. Petersburg, Minsk, Baghdad, Moscow",
+    UTC: "+03:00",
+  },
+  12600: {
+    timeZone: "Iran",
+    UTC: "+03:30",
+  },
+  14400: {
+    timeZone: "Volgograd, Baku, Yerevan",
+    UTC: "+04:00",
+  },
+  16200: {
+    timeZone: "Afghanistan",
+    UTC: "+04:30",
+  },
+  18000: {
+    timeZone: "Yekaterinburg, Tashkent",
+    UTC: "+05:00",
+  },
+  19800: {
+    timeZone: "Chennai, Kolkata, Mumbai, New Delhi",
+    UTC: "+05:30",
+  },
+  20700: {
+    timeZone: "Nepal",
+    UTC: "+05:45",
+  },
+  21600: {
+    timeZone: "Omsk, Almaty",
+    UTC: "+06:00",
+  },
+  23400: {
+    timeZone: "Myanmar, Cocos Islands",
+    UTC: "+06:30",
+  },
+  25200: {
+    timeZone: "-",
+    UTC: "+07:00",
+  },
+  28800: {
+    timeZone: "Krasnoyarsk, Ulaan Bataar, Perth",
+    UTC: "+08:00",
+  },
+  32400: {
+    timeZone: "Irkutsk",
+    UTC: "+09:00",
+  },
+  34200: {
+    timeZone: "Australian Central Standard Time",
+    UTC: "+09:30",
+  },
+  36000: {
+    timeZone: "Yakutsk, Canberra, Melbourne, Sydney, Hobart",
+    UTC: "+10:00",
+  },
+  37800: {
+    timeZone: "Lord Howe Standard Time",
+    UTC: "+10:30",
+  },
+  39600: {
+    timeZone: "Vladivostok, Solomon Is., New Caledonia",
+    UTC: "+11:00",
+  },
+  41400: {
+    timeZone: "Norfolk Islan",
+    UTC: "+11:30",
+  },
+  43200: {
+    timeZone: "Magadan, Auckland, Wellington",
+    UTC: "+12:00",
+  },
+  45900: {
+    timeZone: "New Zealand, Chatham Island",
+    UTC: "+12:45",
+  },
+  46800: {
+    timeZone: "Nuku'alofa",
+    UTC: "+13:00",
+  },
+  50400: {
+    timeZone: "Kiribati, Line Islands",
+    UTC: "+14:00",
+  },
+};
